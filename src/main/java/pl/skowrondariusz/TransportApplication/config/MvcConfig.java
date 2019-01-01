c44d26aaceb194eabc447337a9aab7298d2c47b1
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-01-01 23:40:09 +0100

    Added logging in and spring security. need to change templates and add database for users.

--- src/main/java/pl/skowrondariusz/TransportApplication/config/MvcConfig.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/config/MvcConfig.java
@@ -1,67 +1,17 @@
-//package pl.skowrondariusz.TransportApplication.config;
-//
-//
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//import org.springframework.web.servlet.config.annotation.*;
-//
-//@Configuration
-//@EnableWebMvc
-////@EnableTransactionManagement
-////@ComponentScan(basePackages = "pl.skowrondariusz.TransportApplication")
-//public class MvcConfig extends WebMvcConfigurerAdapter {
-
-//    @Bean
-//    @Primary
-//    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-//        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-//        return builder.modulesToInstall(new JavaTimeModule());
-//    }
+package pl.skowrondariusz.TransportApplication.config;
 
 
-//    @Bean
-//    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
-//        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
-//        resolver.setContentNegotiationManager(manager);
-//
-//        // Define all possible view resolvers
-//        List<ViewResolver> resolvers = new ArrayList<>();
-//
-////        resolvers.add(csvViewResolver());
-////        resolvers.add(excelViewResolver());
-//        resolvers.add(pdfViewResolver());
-//
-//        resolver.setViewResolvers(resolvers);
-//        return resolver;
-//    }
-//
-//    @Bean
-//    public ViewResolver pdfViewResolver() {
-//        return new PdfViewResolver();
-//    }
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
-//        registry.addResourceHandler("/static/images/**").addResourceLocations("classpath:/images/");
-//        registry.addResourceHandler("/script/**").addResourceLocations("classpath:/script/");
-//    }
-//@Override
-//public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//    registry.addResourceHandler(
-//            "webjars/**",
-//            "images/**")
-//            .addResourceLocations(
-//                    "classpath:/META-INF/resources/webjars/",
-//                    "classpath:/static/images/");
-//
-//
-//}
-//
-//}
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
 
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/index").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/indexLog").setViewName("loggedHome");
+        registry.addViewController("/index").setViewName("login");
+    }
+}
