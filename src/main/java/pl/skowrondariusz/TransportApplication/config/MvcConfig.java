a98995322bb812296f3db80877eee834e8a19a89
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-01-03 22:32:43 +0100

    Login working, need to add services for user

--- src/main/java/pl/skowrondariusz/TransportApplication/config/MvcConfig.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/config/MvcConfig.java
@@ -11,7 +11,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/indexLog").setViewName("loggedHome");
+        registry.addViewController("/indexLog").setViewName("indexLog");
         registry.addViewController("/login").setViewName("login");
     }
 }
--- src/main/java/pl/skowrondariusz/TransportApplication/config/WebSecurityConfig.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/config/WebSecurityConfig.java
@@ -29,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+        http.formLogin().defaultSuccessUrl("/indexLog", true);
     }
 
     @Bean
