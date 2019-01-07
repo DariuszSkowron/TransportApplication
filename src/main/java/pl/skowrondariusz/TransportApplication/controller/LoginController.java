da9eedbcfcbe84d823793dfbcd6e6e8530c7f0f0
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-01-07 23:09:52 +0100

    User registration

--- src/main/java/pl/skowrondariusz/TransportApplication/controller/LoginController.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -16,10 +17,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import pl.skowrondariusz.TransportApplication.model.UserDto;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -83,6 +89,31 @@ public class LoginController {
         return "indexLog";
     }
 
+    @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+
+    @RequestMapping(value = "/user/registration", method = RequestMethod.POST)
+    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid final UserDto userDto, final HttpServletRequest request, final Errors errors) {
+        LOGGER.debug("Registering user account with information: {}", userDto);
+
+        final User registered = userService.registerNewUserAccount(userDto);
+        if (registered == null) {
+            // result.rejectValue("email", "message.regError");
+            return new ModelAndView("registration", "user", userDto);
+        }
+        try {
+            final String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), appUrl));
+        } catch (final Exception ex) {
+            LOGGER.warn("Unable to register user", ex);
+            return new ModelAndView("emailError", "user", userDto);
+        }
+        return new ModelAndView("successRegister", "user", userDto);
+    }
 
 //    @RequestMapping(value = "/username", method = RequestMethod.GET)
 //    public String currentUserName(@ModelAttribute Principal principal, ModelMap modelMap) {
