671823ed64ec261d83b266f77a7e56600e7f7928
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-01-15 23:03:58 +0100

    Fixed email confirmation

--- src/main/java/pl/skowrondariusz/TransportApplication/security/UserActivationController.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/security/UserActivationController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/registrationConfirm")
@@ -41,6 +45,52 @@ public class UserActivationController {
         userService.saveRegisteredUser(user);
 
         return "registrationConfirm";
+    }
+
+    @GetMapping
+    public String resendRegistrationToken(Model model){
+        VerificationToken newVerificationToken = verificationTokenRepository.()
+        return "registrationConfirm";
+    }
+
+
+    @RequestMapping
+    public String resendRegistrationToken(@ModelAttribute("resendRegistrationTokenForm") @Valid VerificationTokenResendDto form,
+                                            BindingResult result,
+                                            HttpServletRequest request) {
+
+        if (result.hasErrors()){
+            return "forgot-password";
+        }
+
+        User user = userService.findByEmail(form.getEmail());
+        if (user == null){
+            result.rejectValue("email", null, "We could not find an account for that e-mail address.");
+            return "forgot-password";
+        }
+
+        PasswordResetToken token = new PasswordResetToken();
+        token.setToken(UUID.randomUUID().toString());
+        token.setUser(user);
+        token.setExpiryDate(30);
+        tokenRepository.save(token);
+
+        Mail mail = new Mail();
+        mail.setFrom("no-reply@skowrondariusz.com");
+        mail.setTo(user.getEmail());
+        mail.setSubject("Password reset request");
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("token", token);
+        model.put("user", user);
+        model.put("signature", "https://skowrondariusz.com");
+        String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        model.put("resetUrl", url + "/reset-password?token=" + token.getToken());
+        mail.setModel(model);
+        emailService.sendEmail(mail);
+
+        return "redirect:/forgot-password?success";
+
     }
 
 
---
+++ src/main/java/pl/skowrondariusz/TransportApplication/security/VerificationTokenResendDto.java
@@ -0,0 +1,22 @@
+package pl.skowrondariusz.TransportApplication.security;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class VerificationTokenResendDto {
+
+        @Email
+        @NotEmpty
+        private String email;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+}
+
+
