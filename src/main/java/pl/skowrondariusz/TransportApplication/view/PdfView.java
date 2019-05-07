3f1609549b058c1de468fc5c8f043fc0c7c693ef
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-05-07 23:58:57 +0200

    exporting files changes

--- src/main/java/pl/skowrondariusz/TransportApplication/view/PdfView.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/view/PdfView.java
@@ -4,6 +4,7 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.skowrondariusz.TransportApplication.model.Transit;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,20 +16,21 @@ import java.util.List;
 import java.util.Map;
 
 public class PdfView extends AbstractPdfView {
+
+
+
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // change the file name
+
         response.setHeader("Content-Disposition", "attachment; filename=\"my-pdf-file.pdf\"");
 
         List<Transit> transits = (List<Transit>) model.get("transitList");
         document.add(new Paragraph("Generated Transits " + LocalDate.now()));
 
-
-//        PdfPTable table = new PdfPTable(transits.stream().findAny().get().getColumnCount());
         PdfPTable table = new PdfPTable(4);
         table.setWidthPercentage(100.0f);
         table.setSpacingBefore(10);
-        Path path = Paths.get(ClassLoader.getSystemResource("static/Skowron.png").toURI());
+        Path path = Paths.get(ClassLoader.getSystemResource("static/logo.jpg").toURI());
         Image img = Image.getInstance(path.toAbsolutePath().toString());
         img.scalePercent(100);
 
@@ -59,33 +61,13 @@ public class PdfView extends AbstractPdfView {
 
         cell.setPhrase(new Phrase("Distance", font));
         table.addCell(cell);
-//
-//        cell.setPhrase(new Phrase("Company", font));
-//        table.addCell(cell);
-//
-//        cell.setPhrase(new Phrase("Address", font));
-//        table.addCell(cell);
-//
-//        cell.setPhrase(new Phrase("City", font));
-//        table.addCell(cell);
-//
-//        cell.setPhrase(new Phrase("Country", font));
-//        table.addCell(cell);
-//
-//        cell.setPhrase(new Phrase("Phone Number", font));
-//        table.addCell(cell);
+
 
         for(Transit transit : transits){
             table.addCell(transit.getId().toString());
             table.addCell(transit.getSourceAdress());
             table.addCell(transit.getDestinationAdress());
             table.addCell(transit.getDistance().toString());
-//            table.addCell(user.getCompany());
-//            table.addCell(user.getAddress());
-//            table.addCell(user.getCity());
-//            table.addCell(user.getCountry());
-//            table.addCell(user.getPhoneNumber());
-
         }
 
         document.add(table);
