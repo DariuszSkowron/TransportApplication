2f2a977ab2a36bf2d4ac3a722465c44ce94c4120
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-05-09 23:58:20 +0200

    email sending changes

--- src/main/java/pl/skowrondariusz/TransportApplication/security/service/EmailService.java
+++ src/main/java/pl/skowrondariusz/TransportApplication/security/service/EmailService.java
@@ -66,30 +66,7 @@ public class EmailService {
         }
     }
 
-//    @Async
-//    public void sendEmail(Mail mail) {
-//        try {
-//            MimeMessage message = emailSender.createMimeMessage();
-//            MimeMessageHelper helper = new MimeMessageHelper(message,
-//                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-//                    StandardCharsets.UTF_8.name());
-//
-//            Context context = new Context();
-//            context.setVariables(mail.getModel());
-//            String html = templateEngine.process("email/email-template", context);
-//
-//            helper.setTo(mail.getTo());
-//            helper.setText(html, true);
-//            helper.setSubject(mail.getSubject());
-//            helper.setFrom(mail.getFrom());
-//
-//            emailSender.send(message);
-//        } catch (Exception e){
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//
+
 
 
 
