5f797d72ec100d6b640a95f4f6bdb9eefcedcf99
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-05-19 23:20:04 +0200

    changes to tests

--- src/test/java/pl/skowrondariusz/TransportApplication/api/ApiTransitControllerTest.java
+++ src/test/java/pl/skowrondariusz/TransportApplication/api/ApiTransitControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -45,6 +46,7 @@ public class ApiTransitControllerTest {
     @MockBean
     private TransitService transitService;
     
+    @WithMockUser(value = "spring")
     @Test
     public void shouldAddTransitAndCalculateDistance() throws Exception {
 
@@ -59,6 +61,7 @@ public class ApiTransitControllerTest {
                 .andExpect(jsonPath("$[0].date", is("2018-12-05")));
     }
     
+    @WithMockUser(value = "spring")
     @Test
     public void shouldCreateBook() throws Exception {
 
@@ -76,6 +79,7 @@ public class ApiTransitControllerTest {
         verify(transitService).addTransit(eq(new Transit("Poznań", "Kraków", 12d, LocalDate.of(2018, 12, 5))));
     }
     
+    @WithMockUser(value = "spring")
     @Test
     public void shouldGetTransitFromId() throws Exception {
 
